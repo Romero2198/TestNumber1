5e1cef181ccd66d0fb92d186345771108baec087
Yung.Energy <morskojroma@gmail.com>
2022-08-15 19:45:26 +0300

    Add Method ArrayConditionCheck

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,14 @@
 ﻿string[] array = new string[4] {"hello", "2", "world", ";-)"};
 string[] array1 = new string[array.Length];
+void ArrayConditionCheck(string[] array, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array1[count] = array[i];
+        count++;
+        }
+    }
+}
